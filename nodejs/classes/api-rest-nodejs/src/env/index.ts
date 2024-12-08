6829c6ad90d8e2b482885e256e2ac5a26f6e277f
Eder Silva <ederf-silva@hotmail.com>
2024-12-07 21:49:44 -0300

    refactor: :recycle: Adiciona sqlite como default no envSchema

--- nodejs/classes/api-rest-nodejs/src/env/index.ts
+++ nodejs/classes/api-rest-nodejs/src/env/index.ts
@@ -14,7 +14,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['production', 'test', 'development']).default('production'),
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.string(),
-  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   SECRET_KEY: z.string(),
 });
 
